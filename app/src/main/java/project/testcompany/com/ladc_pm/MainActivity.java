66208ec5d45cd332298df33568cff144d0a0b013
Gursel <gursel@live.be>
2018-12-20 00:07:57 +0100

    Change Background Color Gürsel Klaar

--- app/src/main/java/project/testcompany/com/ladc_pm/MainActivity.java
+++ app/src/main/java/project/testcompany/com/ladc_pm/MainActivity.java
@@ -1,11 +1,15 @@
 package project.testcompany.com.ladc_pm;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.crashlytics.android.Crashlytics;
+
+import java.util.Random;
+
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,14 +21,20 @@ public class MainActivity extends AppCompatActivity {
         startActivity(KeremActivity);
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
     }
 
+    View view;
+    Random rnd = new Random();
     public void ActivityGursel(View view) {
-        
+        view = this.getWindow().getDecorView();
+        view.setBackgroundColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
+
     }
 }
