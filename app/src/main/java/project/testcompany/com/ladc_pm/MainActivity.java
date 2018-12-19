fa53b9d8a42e696427986ce425dbc6077be73dfd
YilmazKerem <s100757@ap.be>
2018-12-19 22:32:22 +0100

    SoundBoard Kerem Klaar

--- app/src/main/java/project/testcompany/com/ladc_pm/MainActivity.java
+++ app/src/main/java/project/testcompany/com/ladc_pm/MainActivity.java
@@ -1,13 +1,26 @@
 package project.testcompany.com.ladc_pm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void ActivityKerem(View view)
+    {
+        Intent KeremActivity = new Intent(this,SoundBoardActivity.class);
+
+        startActivity(KeremActivity);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/project/testcompany/com/ladc_pm/SoundBoardActivity.java
+++ app/src/main/java/project/testcompany/com/ladc_pm/SoundBoardActivity.java
@@ -1,7 +1,10 @@
 package project.testcompany.com.ladc_pm;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SoundBoardActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class SoundBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound_board);
+
+
+        final MediaPlayer PlayBoxsound= MediaPlayer.create(this, R.raw.roblox_death_sound_1);
+
+        final Button PlaySound = (Button) this.findViewById(R.id.Button_PlaySound);
+
+        PlaySound.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v)
+           {
+               PlayBoxsound.start();
+           }
+        });
+
     }
 }
